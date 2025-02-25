f23e699f16cac416bfb34d294f920870c7a04ced
HATower <108533836+HATower@users.noreply.github.com>
2025-02-25 15:55:17 +0100

    correctipon Fetch dtime

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ PRICES_FILE = "prices.json"
 fetch_times = [
         datetime.strptime("07:30", "%H:%M").time(),
         datetime.strptime("11:00", "%H:%M").time(),
-        datetime.strptime("14:54", "%H:%M").time(),
+        datetime.strptime("14:56", "%H:%M").time(),
         datetime.strptime("16:00", "%H:%M").time(),
     ]
 # Function to fetch data from the API
@@ -52,10 +52,9 @@ def get_prices():
 gold_price_per_gram, silver_price_per_gram, last_fetch_time = get_prices()
 def is_time_to_fetch():
     current_time = datetime.now().time()
-    st.write('TESTHours'+datetime.strptime(current_time,"%H:%M").time())
-
+    st.write('TESTHours'+str(current_time))
     return current_time in fetch_times
-if is_time_to_fetch() in fetch_times or datetime.now() - last_fetch_time > timedelta(days=1):
+if is_time_to_fetch() or datetime.now() - last_fetch_time > timedelta(days=1):
                 gold_price_per_gram, silver_price_per_gram = fetch_data_from_api()
 # Function to load all user data
 def load_all_data():
